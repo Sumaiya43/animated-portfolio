8e735a27d083f7da831e6ad02940feb648720ece
Sumaiya Begum <sumaiyabegum43@gmail.com>
2024-04-22 00:54:23 -0700

    parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return <div>
-    <section>
+    <section id="Homepage">
       <Navbar/>
       <Hero/>
     </section>
-    <section >Parallax</section>
-    <section id="Services">Service</section>
-    <section >Parallax</section>
+    <section ><Parallax type="services"/></section>
+    <section id="Services">Services</section>
+    <section ><Parallax type="portfolio" /></section>
     <section id="Porfolio">Porfolio1</section>
     <section>Porfolio2</section>
     <section>Porfolio3</section>
